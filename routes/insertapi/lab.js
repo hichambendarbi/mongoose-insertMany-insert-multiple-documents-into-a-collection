7aa6a21a7d90fbd45b9191ce81bd9fe569edb164
khmmamed <khmammed@gmail.com>
2020-02-15 18:16:27 +0100

    v1

--- routes/insertapi/lab.js
+++ routes/insertapi/lab.js
@@ -5,15 +5,35 @@ const config = require("config");
 const dbLabs = config.get("labs");
 //  @route   POST insertapi/lab
 //  @desc    insert all labs
-route.post("/", (req, res) => {
+route.post("/", async (req, res) => {
   //   const doc1 = { name: "hicham" };
   //   const doc2 = { name: "simo" };
+  try {
+    var newLab = [];
+    for (var i = 0; i < dbLabs.length; i++) {
+      newLab = new Lab({
+        identification: {
+          name: dbLabs[i].name
+        },
+        contact: {
+          tele: dbLabs[i].tele,
+          fax: dbLabs[i].fax,
+          city: dbLabs[i].city,
+          adress: dbLabs[i].adress
+        }
+      });
+      await newLab.save();
+      console.log(newLab);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 
-  return Lab.insertMany(dbLabs)
-    .then(result => {
-      return res.json(result);
-    })
-    .catch(err => console.error(`Failed to insert documents: ${err}`));
+  // return Lab.insertMany(dbLabs)
+  //   .then(result => {
+  //     return res.json(result);
+  //   })
+  //   .catch(err => console.error(`Failed to insert documents: ${err}`));
 });
 
 module.exports = route;
